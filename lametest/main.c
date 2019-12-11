9a876c61291ad5c3c73d396e8782a2c39ffe025f
Lucius Kwok <lk@felttip.com>
2019-12-10 23:38:04 -0500

    Initial Commit

---
+++ lametest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lametest
+//
+//  Created by Lucius Kwok on 12/10/19.
+//  Copyright © 2019 Lucius Kwok. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
